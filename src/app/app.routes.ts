a297dc076959e99b1bd2381d87e730c95c24a9ff
Vaibhav <vaibhavsabale1314@gmail.com>
2026-01-08 10:51:56 +0530

    .

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,10 +1,8 @@
 import { Routes } from '@angular/router';
 import { Dashboard } from './dashboard/dashboard';
-import { Home } from './home/home';
 
 export const routes: Routes = [
     { path: '', redirectTo: 'dashboard', pathMatch: 'full' },
     { path: 'dashboard', component: Dashboard },
-    { path: 'home', component: Home }
 
 ];
--- src/app/floor/floor.ts
+++ src/app/floor/floor.ts
@@ -14,6 +14,16 @@ interface Room {
   iconsBottom: string[];
 }
 
+interface Floor {
+  id: string;
+  floorNumber: number;
+  totalRooms: number;
+  cleaned: number;
+  dirty: number;
+  inspect: number;
+  rooms: Room[];
+}
+
 @Component({
   selector: 'app-floor',
   standalone: true,
@@ -21,125 +31,142 @@ interface Room {
   templateUrl: './floor.html',
   styleUrls: ['./floor.css'],
 })
-export class FloorComponent {
 
+export class FloorComponent {
   activeIndex: string[] = ['0'];
 
-  floor1Rooms: Room[] = [
+  floors: Floor[] = [
     {
-      number: '101',
-      type: 'Deluxe',
-      statusClass: 'cleaned',
-      iconsTop: ['departure.svg', 'guest.svg', 'arrival.svg'],
-      iconsBottom: ['guest-req.svg', 'maintenance.svg', 'dnd.svg'],
+      id: '0',
+      floorNumber: 1,
+      totalRooms: 10,
+      cleaned: 5,
+      dirty: 3,
+      inspect: 2,
+      rooms: [
+        {
+          number: '101',
+          type: 'Deluxe',
+          statusClass: 'cleaned',
+          iconsTop: ['departure.svg', 'guest.svg', 'arrival.svg'],
+          iconsBottom: ['guest-req.svg', 'maintenance.svg', 'dnd.svg'],
+        },
+        {
+          number: '102',
+          type: 'Deluxe',
+          statusClass: 'dirty',
+          iconsTop: ['departure.svg', 'guest.svg', 'arrival.svg'],
+          iconsBottom: ['guest-req.svg', 'maintenance.svg'],
+        },
+        {
+          number: '103',
+          type: 'Deluxe',
+          statusClass: 'inspect',
+          iconsTop: ['departure.svg', 'guest.svg', 'arrival.svg'],
+          iconsBottom: ['guest-req.svg', 'maintenance.svg'],
+        },
+        {
+          number: '104',
+          type: 'Deluxe',
+          statusClass: 'cleaned',
+          iconsTop: ['departure.svg', 'guest.svg', 'arrival.svg'],
+          iconsBottom: ['guest-req.svg', 'maintenance.svg', 'dnd.svg'],
+        },
+        {
+          number: '105',
+          type: 'Deluxe',
+          statusClass: 'dirty',
+          iconsTop: ['departure.svg', 'guest.svg', 'arrival.svg'],
+          iconsBottom: ['guest-req.svg', 'maintenance.svg', 'dnd.svg'],
+        },
+        {
+          number: '106',
+          type: 'Deluxe',
+          statusClass: 'dirty',
+          iconsTop: ['departure.svg', 'guest.svg', 'arrival.svg'],
+          iconsBottom: ['guest-req.svg', 'maintenance.svg', 'dnd.svg'],
+        },
+        {
+          number: '107',
+          type: 'Deluxe',
+          statusClass: 'cleaned',
+          iconsTop: ['departure.svg', 'guest.svg', 'arrival.svg'],
+          iconsBottom: ['guest-req.svg', 'maintenance.svg'],
+        },
+        {
+          number: '108',
+          type: 'Deluxe',
+          statusClass: 'cleaned',
+          iconsTop: ['departure.svg', 'guest.svg', 'arrival.svg'],
+          iconsBottom: ['guest-req.svg', 'maintenance.svg'],
+        },
+        {
+          number: '109',
+          type: 'Deluxe',
+          statusClass: 'inspect',
+          iconsTop: ['departure.svg', 'guest.svg', 'arrival.svg'],
+          iconsBottom: ['guest-req.svg', 'maintenance.svg', 'dnd.svg'],
+        },
+        {
+          number: '110',
+          type: 'Deluxe',
+          statusClass: 'cleaned',
+          iconsTop: ['departure.svg', 'guest.svg', 'arrival.svg'],
+          iconsBottom: ['guest-req.svg', 'maintenance.svg', 'dnd.svg'],
+        },
+      ],
     },
     {
-      number: '102',
-      type: 'Deluxe',
-      statusClass: 'dirty',
-      iconsTop: ['departure.svg', 'guest.svg', 'arrival.svg'],
-      iconsBottom: ['guest-req.svg', 'maintenance.svg'],
+      id: '1',
+      floorNumber: 2,
+      totalRooms: 6,
+      cleaned: 2,
+      dirty: 3,
+      inspect: 1,
+      rooms: [
+        {
+          number: '201',
+          type: 'Deluxe',
+          statusClass: 'cleaned',
+          iconsTop: ['departure.svg', 'guest.svg', 'arrival.svg'],
+          iconsBottom: ['guest-req.svg', 'maintenance.svg', 'mur.svg'],
+        },
+        {
+          number: '202',
+          type: 'Deluxe',
+          statusClass: 'dirty',
+          iconsTop: ['departure.svg', 'guest.svg', 'arrival.svg'],
+          iconsBottom: ['guest-req.svg', 'maintenance.svg'],
+        },
+        {
+          number: '203',
+          type: 'Deluxe',
+          statusClass: 'inspect',
+          iconsTop: ['departure.svg', 'guest.svg', 'arrival.svg'],
+          iconsBottom: ['guest-req.svg', 'maintenance.svg'],
+        },
+        {
+          number: '204',
+          type: 'Deluxe',
+          statusClass: 'cleaned',
+          iconsTop: ['departure.svg', 'guest.svg', 'arrival.svg'],
+          iconsBottom: ['guest-req.svg', 'maintenance.svg', 'mur.svg'],
+        },
+        {
+          number: '205',
+          type: 'Deluxe',
+          statusClass: 'dirty',
+          iconsTop: ['departure.svg', 'guest.svg', 'arrival.svg'],
+          iconsBottom: ['guest-req.svg', 'maintenance.svg', 'dnd.svg'],
+        },
+        {
+          number: '206',
+          type: 'Deluxe',
+          statusClass: 'dirty',
+          iconsTop: ['departure.svg', 'guest.svg', 'arrival.svg'],
+          iconsBottom: ['guest-req.svg', 'maintenance.svg', 'dnd.svg'],
+        },
+      ],
     },
-    {
-      number: '103',
-      type: 'Deluxe',
-      statusClass: 'inspect',
-      iconsTop: ['departure.svg', 'guest.svg', 'arrival.svg'],
-      iconsBottom: ['guest-req.svg', 'maintenance.svg'],
-    },
-    {
-      number: '104',
-      type: 'Deluxe',
-      statusClass: 'cleaned',
-      iconsTop: ['departure.svg', 'guest.svg', 'arrival.svg'],
-      iconsBottom: ['guest-req.svg', 'maintenance.svg', 'dnd.svg'],
-    },
-    {
-      number: '105',
-      type: 'Deluxe',
-      statusClass: 'dirty',
-      iconsTop: ['departure.svg', 'guest.svg', 'arrival.svg'],
-      iconsBottom: ['guest-req.svg', 'maintenance.svg', 'dnd.svg'],
-    },
-    {
-      number: '106',
-      type: 'Deluxe',
-      statusClass: 'dirty',
-      iconsTop: ['departure.svg', 'guest.svg', 'arrival.svg'],
-      iconsBottom: ['guest-req.svg', 'maintenance.svg', 'dnd.svg'],
-    },
-    {
-      number: '107',
-      type: 'Deluxe',
-      statusClass: 'cleaned',
-      iconsTop: ['departure.svg', 'guest.svg', 'arrival.svg'],
-      iconsBottom: ['guest-req.svg', 'maintenance.svg'],
-    },
-    {
-      number: '108',
-      type: 'Deluxe',
-      statusClass: 'cleaned',
-      iconsTop: ['departure.svg', 'guest.svg', 'arrival.svg'],
-      iconsBottom: ['guest-req.svg', 'maintenance.svg'],
-    },
-    {
-      number: '109',
-      type: 'Deluxe',
-      statusClass: 'inspect',
-      iconsTop: ['departure.svg', 'guest.svg', 'arrival.svg'],
-      iconsBottom: ['guest-req.svg', 'maintenance.svg', 'dnd.svg'],
-    },
-    {
-      number: '110',
-      type: 'Deluxe',
-      statusClass: 'cleaned',
-      iconsTop: ['departure.svg', 'guest.svg', 'arrival.svg'],
-      iconsBottom: ['guest-req.svg', 'maintenance.svg', 'dnd.svg'],
-    },
-  ];
-
-  floor2Rooms: Room[] = [
-    {
-      number: '201',
-      type: 'Deluxe',
-      statusClass: 'cleaned',
-      iconsTop: ['departure.svg', 'guest.svg', 'arrival.svg'],
-      iconsBottom: ['guest-req.svg', 'maintenance.svg', 'mur.svg'],
-    },
-    {
-      number: '202',
-      type: 'Deluxe',
-      statusClass: 'dirty',
-      iconsTop: ['departure.svg', 'guest.svg', 'arrival.svg'],
-      iconsBottom: ['guest-req.svg', 'maintenance.svg'],
-    },
-    {
-      number: '203',
-      type: 'Deluxe',
-      statusClass: 'inspect',
-      iconsTop: ['departure.svg', 'guest.svg', 'arrival.svg'],
-      iconsBottom: ['guest-req.svg', 'maintenance.svg'],
-    },
-    {
-      number: '204',
-      type: 'Deluxe',
-      statusClass: 'cleaned',
-      iconsTop: ['departure.svg', 'guest.svg', 'arrival.svg'],
-      iconsBottom: ['guest-req.svg', 'maintenance.svg', 'mur.svg'],
-    },
-    {
-      number: '205',
-      type: 'Deluxe',
-      statusClass: 'dirty',
-      iconsTop: ['departure.svg', 'guest.svg', 'arrival.svg'],
-      iconsBottom: ['guest-req.svg', 'maintenance.svg', 'dnd.svg'],
-    },
-    {
-      number: '206',
-      type: 'Deluxe',
-      statusClass: 'dirty',
-      iconsTop: ['departure.svg', 'guest.svg', 'arrival.svg'],
-      iconsBottom: ['guest-req.svg', 'maintenance.svg', 'dnd.svg'],
-    }
   ];
 }
